cb036278d08eb5c615405fa1a76e430d0b56309d
saammirghorbani <95sami81@gmail.com>
2019-04-28 15:29:03 +0200

    Added long running environment

--- gym_goalie/__init__.py
+++ gym_goalie/__init__.py
@@ -6,3 +6,10 @@ register(
     kwargs={'reward_type': 'sparse'},
     max_episode_steps=100
 )
+
+register(
+    id='Goalie-long-v0',
+    entry_point='gym_goalie.envs:GoalieTestEnv',
+    kwargs={'reward_type': 'sparse'},
+    max_episode_steps=2000
+)
